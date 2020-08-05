66ec856a0779f27cc1d7546c533c87184605a253
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-04 20:09:09 -0500

    creating express server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Serving static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//Routes
+app.use(routes);
+
+//Connecting to MYSQL
+
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
