d14bf3034b43e643a52a0490325a7f44dbe7021a
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-06 20:44:14 -0500

    post model updated

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const PostSchema = new Schema({
+  userId: {
+    type: String,
+    trim: true,
+    required: "ID is required.",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Username is required.",
+  },
+  title: {
+    type: String,
+    trim: true,
+    required: "Post title is required.",
+  },
+  content: {
+    type: String,
+    trim: true,
+    required: "Post must not be empty.",
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: "Location is required.",
+  },
+  category: {
+    type: String,
+    trim: true,
+    default: General,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
 module.exports = Post;
