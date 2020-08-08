0d22493a57bed86eb8914b28ea75414280a8bde6
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-08 13:15:11 -0500

    adding message and post routes to easy export file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const userRoute = require("./user");
+const postRoute = require("./post");
+const messageRoute = require("./message");
 
 //User routes
 router.use("/user", userRoute);
+
+//Post routes
+router.use("/post", postRoute);
+
+//Message routes
+router.use("/message", messageRoute);
 
 module.exports = router;
