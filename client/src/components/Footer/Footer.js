86cd11534d2fd984a16994c39197ee864b6914ee
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-11 18:17:37 -0500

    fixed issue

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Project 4 Made with ❤️
+        Project 3 Made with ❤️
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
