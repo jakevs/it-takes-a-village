8d502ebed0d16614f29a10272183082cee3d24ba
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-11 18:28:27 -0500

    passport config file completed

--- passport/setup.js
+++ passport/setup.js
@@ -1,5 +1,5 @@
-const User = require("../models/User");
 const passport = require("passport");
+const db = require("../models");
 const LocalStrategy = require("passport-local").Strategy;
 
 // Serialize is used to keep user logged in from page to page.
@@ -7,9 +7,29 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
+  db.User.findById(id, (err, user) => {
     done(err, user);
   });
 });
 
 //Local Strategy
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({
+      where: {
+        username: username,
+      },
+    }).then((dbUser) => {
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Provide a valid username.",
+        });
+      } else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password.",
+        });
+      }
+      return done(null, dbUser);
+    });
+  })
+);
