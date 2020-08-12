3ec91805911068c56d73c44bebc49606d776425f
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-11 19:48:21 -0500

    got rid of unused code

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import Nav from "./components/Nav/Header";
 import Footer from "./components/Footer/Footer";
 import Post from "./components/Posts/Index";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Nav />
-      </header>
+      <Nav />
       <Post />
       <Footer />
     </div>
