1cf2f3ff3e50c272900ed0a0afa93df7541c09b1
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-12 21:38:28 -0500

    added pages folder and profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./components/Nav/Header";
+import Footer from "./components/Footer/Footer";
+
+function Profile() {
+  return (
+    <div className="App">
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+export default Profile;
