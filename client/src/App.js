6cd7c07af2873ff2e1a759cca336f28c14cc8b40
wslifer <wesleyslifer@ymail.com>
2020-08-13 19:45:59 -0500

    moving post component to seperate page and installing react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "./components/Nav/Header";
 import Footer from "./components/Footer/Footer";
-import Post from "./components/Posts/";
+import Post from "./pages/Postpage";
 
 function App() {
   return (
