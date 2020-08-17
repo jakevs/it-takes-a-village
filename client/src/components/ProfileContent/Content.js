e0c603b205633556ad66b9119812b0dfadbd5dd4
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-17 18:00:44 -0500

    making styling changes for profile

--- client/src/components/ProfileContent/Content.js
+++ client/src/components/ProfileContent/Content.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -20,8 +21,9 @@ const useStyles = makeStyles({
     backgroundColor: "#2E8B57"
   },
   media: {
-    justifyContent: "center",
-    height: 400
+    height: 0,
+    paddingTop: "56.25%", // 16:9,
+    marginTop: "30"
   },
   title: {
     fontFamily: "'Antic Slab', serif",
@@ -42,71 +44,74 @@ export default function MediaCard() {
   };
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://www.impact.upenn.edu/wp-content/uploads/2020/04/covid-graphic_640x400_acf_cropped.png"
-          title="Covid Help"
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            className={classes.title}
-            variant="h5"
-            component="h2"
+    <Container maxWidth="sm">
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://www.impact.upenn.edu/wp-content/uploads/2020/04/covid-graphic_640x400_acf_cropped.png"
+            title="Covid Help"
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              className={classes.title}
+              variant="h5"
+              component="h2"
+            >
+              My Skills
+            </Typography>
+            <Typography variant="body2" className={classes.title} component="p">
+              What skills can I offer to my community during this time of
+              crisis? While many people are stuck at home, or out of work, we
+              still have many skills that we can contribute to our community.
+              Here you can add skills so other members of the community will
+              know what you can do to help!
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button className={classes.title} onClick={handleClickOpen}>
+            Add Skill
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="form-dialog-title"
           >
-            My Skills
-          </Typography>
-          <Typography variant="body2" className={classes.title} component="p">
-            What skills can I offer to my community during this time of crisis?
-            While many people are stuck at home, or out of work, we still have
-            many skills that we can contribute to our community. Here you can
-            add skills so other members of the community will know what you can
-            do to help!
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button
-          variant="outlined"
-          className={classes.title}
-          onClick={handleClickOpen}
-        >
-          Add Skill
-        </Button>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">
-            Add Skills to your Profile!
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText>Add your skills here</DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Skill"
-              type="email"
-              fullWidth
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={handleClose} color="primary">
-              Add{" "}
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Button size="small" className={classes.title} to="/" component={Link}>
-          Dashboard{" "}
-        </Button>
-      </CardActions>
-    </Card>
+            <DialogTitle id="form-dialog-title">
+              Add Skills to your Profile!
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>Add your skills here</DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Skill"
+                type="email"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleClose} color="primary">
+                Add{" "}
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Button
+            size="small"
+            className={classes.title}
+            to="/"
+            component={Link}
+          >
+            Dashboard{" "}
+          </Button>
+        </CardActions>
+      </Card>
+    </Container>
   );
 }
