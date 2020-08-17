093b56ec82bd7dec23da1f26ec3e2c14a6866a97
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-17 17:13:12 -0500

    added button functionality to skills

--- client/src/components/ProfileContent/Content.js
+++ client/src/components/ProfileContent/Content.js
@@ -7,6 +7,12 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
@@ -25,6 +31,15 @@ const useStyles = makeStyles({
 
 export default function MediaCard() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Card className={classes.root}>
@@ -53,9 +68,41 @@ export default function MediaCard() {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" className={classes.title}>
+        <Button
+          variant="outlined"
+          className={classes.title}
+          onClick={handleClickOpen}
+        >
           Add Skill
         </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">
+            Add Skills to your Profile!
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>Add your skills here</DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Skill"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Add{" "}
+            </Button>
+          </DialogActions>
+        </Dialog>
         <Button size="small" className={classes.title} to="/" component={Link}>
           Dashboard{" "}
         </Button>
