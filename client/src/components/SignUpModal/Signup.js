3b1c066f36475d64f5daaa186b001b592d8e6dbd
Holly Prothe <hollyprothe1@gmail.com>
2020-08-18 20:43:31 -0500

    remove redundant code

--- client/src/components/SignUpModal/Signup.js
+++ client/src/components/SignUpModal/Signup.js
@@ -50,17 +50,16 @@ class SUModal extends React.Component {
             };
 
             fetch("/api/users", {
-                method: "POST", // *GET, POST, PUT, DELETE, etc.
-                mode: "cors", // no-cors, cors, *same-origin
-                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: "same-origin", // include, *same-origin, omit
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
                 headers: {
-                    "Content-Type": "application/json",
-                    // "Content-Type": "application/x-www-form-urlencoded",
+                    "Content-Type": "application/json"
                 },
-                redirect: "follow", // manual, *follow, error
-                referrer: "no-referrer", // no-referrer, *client
-                body: JSON.stringify(data), // body data type must match "Content-Type" header
+                redirect: "follow",
+                referrer: "no-referrer",
+                body: JSON.stringify(data),
             })
                 .then(response => {
                     if (response.status === 401) {
