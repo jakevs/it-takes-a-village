a305b7642a293cb0b2d49c2e639ea285eca79a13
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-18 21:24:11 -0500

    fixing skill api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./user");
 const postRoutes = require("./post");
 const messageRoutes = require("./message");
+const skillRoutes = require("./skills");
 
 //User routes
 router.use("/user", userRoutes);
@@ -11,5 +12,8 @@ router.use("/post", postRoutes);
 
 //Message routes
 router.use("/message", messageRoutes);
+
+//Skill routes
+router.use("/skill", skillRoutes);
 
 module.exports = router;
