64cb20ff8d6bea9ab208763394b544b9588d6497
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-18 21:21:50 -0500

    adding skill api, skill api route, and updated model to export

---
+++ routes/api/skills.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const skillController = require("../../controllers/skillController");
+
+router.route("/").get(skillController.findAll).post(skillController.create);
+
+router.delete(skillController.remove);
+
+module.exports = router;
